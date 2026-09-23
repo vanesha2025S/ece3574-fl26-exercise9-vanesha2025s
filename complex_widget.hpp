773c264f078163ee3b833017c2439e3173d50c09
Vanesha Hari <pid@vt.edu>
2026-09-23 17:45:11 -0400

    now i updated complex_widget.hpp

--- complex_widget.hpp
+++ complex_widget.hpp
@@ -3,6 +3,34 @@
 //
 #ifndef COMPLEX_WIDGET_H
 #define COMPLEX_WIDGET_H
+#include <QWidget>
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
 
+class ComplexWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ComplexWidget(QWidget *parent = nullptr);
+
+    double getRealPart() const;
+    double getImaginaryPart() const;
+    double getMagnitude() const;
+    double getPhase() const;
+
+private slots:
+    void updateDisplay();
+
+private:
+    QLineEdit *realEdit;
+    QLineEdit *imaginaryEdit;
+
+    QLabel *magnitudeLabel;
+    QLabel *phaseLabel;
+
+    QPushButton *updateButton;
+};
 
 #endif
