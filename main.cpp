5d552001a6488fcaee57701773f30cc7d0b73e4b
Vanesha Hari <pid@vt.edu>
2026-09-23 17:57:21 -0400

    This is the main file coded.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 // ECE 3574 - Exercise 9: Qt widgets
 //
 
+#include <QApplication>
+#include "complex_widget.hpp"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+    QApplication app(argc, argv);
 
+    ComplexWidget widget;
+    widget.show();
 
+    return app.exec();
 }
